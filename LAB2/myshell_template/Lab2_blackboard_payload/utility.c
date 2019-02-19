5bfdac252eedb713de4863d4fd5a01f2bc7c5a38
Cassidy Linhares <30815527+cassidylinhares@users.noreply.github.com>
2019-02-19 17:31:44 -0500

    q1 fucntions

--- LAB2/myshell_template/Lab2_blackboard_payload/utility.c
+++ LAB2/myshell_template/Lab2_blackboard_payload/utility.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2017, <GROUP MEMBERS>
  * All rights reserved.
- * 
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,3 +15,34 @@
 
 // Define your utility functions here, these will most likely be functions that you call
 // in your myshell.c source file
+
+void pauseShell(){
+    //while the enter key has not been pressed, sleep
+    while(getchar() != '\n'){
+        printf("%s\n","Paused ...");
+        sleep(10);
+    }
+}
+void helpManual(){
+    //I didnt do the more command but this is just the help command
+    printf("Commands Available:   \n");
+    printf("echo: prints to screen\n");
+    printf("pause: pauses command shell\n");
+    printf("quit: exits shell\n");
+    printf("cd: changes directory\n");
+    printf("clr: clears screen\n");
+    printf("dir: displays contents of directory\n");
+    printf("environ: displays all environments\n\n");
+}
+
+void listEnv(){
+    //list all the environemnts on the user PATH (didnt do home or root)
+    printf("PATH : %s\n", getenv("PATH"));
+}
+
+void clear(){
+    //print 50 blank lines to clear the screen
+    for(int i=0; i< 50; i++){
+        printf("\n");
+    }
+}
